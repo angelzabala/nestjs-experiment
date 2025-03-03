0586c249acbfbaebf4417c9f91b1a6a6c28a73b1
angel chavez <angeldchz@gmail.com>
2025-03-03 14:37:27 -0400

    add custom zod validation pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,31 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './common/database/database.module';
 import { UsersModule } from './components/users/users.module';
+import { APP_PIPE } from '@nestjs/core';
+import { ZodValidationPipe } from 'nestjs-zod';
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { ArgumentMetadata, PipeTransform } from '@nestjs/common';
+import { z, ZodSchema } from 'zod';
+
+export class CustomZodValidationPipe extends ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodSchema) {
+    super();
+  }
+
+  transform(value: any, metadata: ArgumentMetadata) {
+    try {
+      return this.schema.parse(value);
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        throw new HttpException({
+          message: 'Validation failed',
+          errors: error.errors,
+        }, HttpStatus.UNPROCESSABLE_ENTITY); // 422
+      }
+      throw new HttpException('Validation failed', HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+  }
+}
 
 @Module({
   imports: [
@@ -15,6 +40,12 @@ import { UsersModule } from './components/users/users.module';
     UsersModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useClass: CustomZodValidationPipe,
+    },
+  ],
 })
 export class AppModule {}
